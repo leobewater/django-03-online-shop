61540fcd0164ccbca4ea417f0eec089a28d8297a
leobewater <mmbee888@gmail.com>
2024-07-08 15:10:48 -0400

    add product and category to admin

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'slug',
+        'price',
+        'available',
+        'created',
+        'updated'
+    ]
+    list_filter = ['available', 'created', 'updated']
+    # allow to edit multiple rows on listing view at once
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
