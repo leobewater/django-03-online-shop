e2f6019250fdcee994d16314af1794ce69e62cfe
leobewater <mmbee888@gmail.com>
2024-07-09 11:35:51 -0400

    add quantity form and view

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+from shop.models import Product
 
-# Create your views here.
+from .cart import Cart
+from .forms import CartAddProductForm
+
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(
+            product=product,
+            quantity=cd['quantity'],
+            override_quantity=cd['override']
+        )
+    return redirect('cart:cart_detail')
