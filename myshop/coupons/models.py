f91eda48453aaa41e45ea90b94aec9f2dbbb16f8
leobewater <mmbee888@gmail.com>
2024-07-10 11:51:16 -0400

    add coupons to admin

--- myshop/coupons/models.py
+++ myshop/coupons/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Coupon(models.Model):
-    code = models.CharField(max_length=50, unique=True),
+    code = models.CharField(max_length=50, unique=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField(
